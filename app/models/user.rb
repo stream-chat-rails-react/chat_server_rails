25d675a162f8fdb64cb2a4fb7ea2d19305fd5d5f
Phil DeLong <philjdelong@gmail.com>
2020-06-15 11:26:33 -0600

    Write user validations for :username :email :pword

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+	validates_presence_of :username
+	validates_presence_of :email
+	validates_uniqueness_of :email
+	validates_presence_of :password_digest
+	validates_presence_of :chat_token
 end
