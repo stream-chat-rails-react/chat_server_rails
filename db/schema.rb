b1894a2f0be9b12950a5114134085009a67df3c7
Phil DeLong <philjdelong@gmail.com>
2020-06-15 11:25:49 -0600

    Add email to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_165214) do
+ActiveRecord::Schema.define(version: 2020_06_15_172359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_06_15_165214) do
     t.string "chat_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
