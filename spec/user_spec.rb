8e454fcaae9bb5ebc8b0c64f069bc8867bdf903c
Phil DeLong <philjdelong@gmail.com>
2020-06-15 11:19:29 -0600

    Create test for user validations

---
+++ spec/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+	describe "validations" do
+		it { should validate_presence_of(:username) }
+		it { should validate_uniqueness_of(:username) }
+		it { should validate_presence_of(:password_digest) }
+		it { should validate_presence_of(:chat_token) }
+	end
+end
