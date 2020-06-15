14169d69c6c3affaf033deb22400e798698cd527
Phil DeLong <philjdelong@gmail.com>
2020-06-15 17:41:57 -0600

    Write user_controller methods: create, show

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,35 @@
+class Api::V1::UsersController < ApplicationController
+	def create
+		user = User.create(user_params)
+		unless user.save
+			render json: {
+				error: "Information not valid."
+			},
+			status: 400
+		else
+			render json: {
+				success: "User successfully created!"
+			},
+			status: 200
+		end
+
+		def show
+			user = User.find_by(username: params[:username])
+			unless user
+				render json: {
+					error: "Missing or incorrect authentication credentials."
+				},
+				status: 401
+			else
+				render json: UserSerializer.new(user),
+				status: 200
+			end
+		end
+	end
+
+	private
+
+	def user_params
+		params.permit(:username, :email, :password, :password_confirmation)
+	end
+end
