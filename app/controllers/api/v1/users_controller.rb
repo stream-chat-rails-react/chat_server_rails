f1a3b5f0ce99002239a6b1463732482f8d867a8a
Phil DeLong <phildelong@Phils-MacBook-Pro.local>
2020-06-24 11:47:25 -0600

    Rename show method to index, remove serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,36 +1,59 @@
 class Api::V1::UsersController < ApplicationController
-	def create
-		user = User.create(user_params)
-		user.save
-		if user.save
-			render json: {
-				success: "User successfully created!"
-			},
-			status: 200
-		else
-			render json: {
-				error: "Information not valid."
-			},
-			status: 400
-		end
-	end
+	################### need to investigate React Routes ###################
+	# def create
+	# 	user = User.create(user_params)
+	# 	user.save
+	# 	if user.save
+	# 		render json: {
+	# 			"id": "#{user.id}",
+	# 			"email": "#{user.email}",
+	# 			"token": user.chat_token
+	# 		},
+	# 		status: 200
+	# 	else
+	# 		render json: {
+	# 			error: "Information not valid."
+	# 		},
+	# 		status: 400
+	# 	end
+	# end
 
-	def show
-		user = User.find_by(email: params[:email])
-		if user
-			render json: UserSerializer.new(user),
+	def index
+		user = User.find_by(username: params[:username])
+		if user && user.authenticate(params[:password])
+			render json: {
+				"id": user.username,
+				"username": user.username,
+				"token": user.chat_token
+			},
 			status: 200
-		else
+		elsif user
 			render json: {
 				error: "Missing or incorrect authentication credentials."
 			},
 			status: 401
+		else
+			user = User.create(user_params)
+			user.save
+			if user.save
+				render json: {
+					"id": user.id.to_s,
+					"username": user.username,
+					"token": user.chat_token
+				},
+				status: 200
+			else
+				render json: {
+					error: "User not saved."
+				},
+				status: 400
+			end
 		end
 	end
-
+		
 	private
 
 	def user_params
-		params.permit(:username, :email, :password, :password_confirmation)
+		params.permit(:username, :email, :password)
 	end
 end
